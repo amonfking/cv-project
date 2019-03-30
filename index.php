c7d1a39eed3c5783166a3f5bc7cc052b174902ee
amonfking <amonfromdharan@gmail.com>
2019-03-30 11:47:16 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" href="css/style.css"/>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-offset-2 col-lg-8">
+      <section class="panel panel-default">
+        <div class="panel-body">
+          <article class="panel-body">
+            <figure class="text-center">
+              <figcaption>
+                <h3>Aman Kumar Dev</h3> Bagmati, Kathmandu, Sankhamul.
+                <br> Mobile. 9808748984 E-mail: amonfromdharan@gmail.com
+              </figcaption>
+            </figure>
+          </article>
+          <br>
+          <article>
+            <h4>
+            									  <strong>Personal Details</strong>
+								            </h4>
+            <hr>
+            <dl class="dl-horizontal">
+              <dt>Date of Birth:</dt>
+              <dd>December 11, 1997</dd>
+              <dt>Age:</dt>
+              <dd>23</dd>
+              <dt>Marital Status:</dt>
+              <dd>Single</dd>
+              <dt>Interests:</dt>
+              <dd>HTML5, CSS3, PHP, JavaScript, jQuery, Web Design, Responsive Design, Node.js, Java, android and Linux.</dd>
+            </dl>
+          </article>
+
+          <article>
+            <h4>
+									              <strong>Skills</strong>
+								            </h4>
+            <hr>
+            <dl class="dl-horizontal">
+              <dt>Languages:</dt>
+              <dd>HTML5, CSS3, PHP and JavaScript</dd>
+              <dt>Framework:</dt>
+              <dd>jQuery, Bootstrap, NW.js</dd>
+            </dl>
+          </article>
+          
+                    <article>
+            <h4>
+									              <strong>Academic Qualification</strong>
+								            </h4>
+            <hr>
+              <dl class="dl-horizontal">
+              <dt>Present</dt>
+              <dd>Bachelor in IT | Softwarica College of IT and E-Commerce, Kathmandu, Nepal (Final Year Running)</dd>
+            </dl>
+            
+            
+            <dl class="dl-horizontal">
+              <dt>2015</dt>
+              <dd>+2 | Mod Narayan College. Supaul, Bihar, India </dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>2012</dt>
+              <dd>SLC | Navodaya Vidya Mandir English Boarding High School. Dharan, Sunsari, Nepal </dd>
+            </dl>
+
+          </article>
+
+          <article>
+            <h4>
+									         <strong>Training</strong>
+								          </h4>
+            <dl class="dl-horizontal">
+              <dd>Successfully completed basic computer hardware and software training from Everest
+Computer Institute, Dharan</dd>
+              
+            </dl>
+            <hr>
+          </article>
+
+        </div>
+      </section>
+    </div>
+  </div>
+</div>
